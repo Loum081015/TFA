d608d4192377036d641194675ccdadd20be91c2c
Loum081015 <lea.perraux@student.heaj.be>
2025-07-29 18:16:28 +0200

    v4

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -11,6 +11,10 @@ let currentImageIndex = 0;
 let isAnimating = false;
 let popupImage = document.querySelector('.popup__img');
 let popupImgElement = popupImage?.querySelector('img');
+let irisJsonData = [];
+const irisButtons = document.querySelectorAll(".iris__btn");
+const irisPopupImage = document.querySelector(".popup__content");
+const irisPopupImageElement = irisPopupImage?.querySelector(".preview-image");
 
 /*Cartes (code inspiré d'un code pen) */
 function animateCartes(newIndex) {
@@ -102,26 +106,68 @@ function scrollListener() {
 }
 
 /*Design fiction*/
-images?.forEach((image, index) => {
-    image.addEventListener("click", () => {
-        popupImage?.classList.add("open");
+
+fetch("assets/fonctionnalite.json")
+    .then((response) => response.json())
+    .then((data) => {
+        irisJsonData = data.images;
+    });
+
+irisButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        irisPopupImage?.classList.add('open');
         document.body.classList.add('no-scroll');
-        showImage(index);
+        showIrisImage(index);
+    //     if(!irisPopupImage.src.includes(irisJsonData[index].src)) {
+    //         irisPopupImage.src = irisJsonData[index].src;
+    //         irisPopupImage.alt = irisJsonData[index].alt;
+    //         irisPopupImage.classList.add('open');
+    //         document.body.classList.add('no-scroll');
+    //         return;
+    //     }
+
+    //     irisPopupImage.src = "";
+    //     irisPopupImage.alt = "";
+    //     irisPopupImage.classList.remove('open');
+    //     document.body.classList.remove('no-scroll');
     });
 });
-popupImage?.addEventListener("click", () => {
-    popupImgElement?.classList.remove('open');
-    popupImgElement?.addEventListener('transitionend', () => {
-        if(popupImgElement?.classList.contains('open')) return;
-        popupImage?.classList.remove("open");
+irisPopupImage?.addEventListener("click", () => {
+    irisPopupImageElement?.classList.remove('open');
+    irisPopupImageElement?.addEventListener('transitionend', () => {
+        if(irisPopupImageElement?.classList.contains('open')) return;
+        irisPopupImage?.classList.remove("open");
         document.body.classList.remove('no-scroll');
     });
 });
-function showImage(index) {
-    popupImgElement.src = images[index].getAttribute('src');
-    popupImgElement.classList.add('open');
+function showIrisImage(index) {
+    irisPopupImageElement.src = irisJsonData[index].src;
+    irisPopupImageElement.alt = irisJsonData[index].alt;
+    irisPopupImageElement.classList.add('open');
     currentImageIndex = index;
 }
+
+
+// images?.forEach((image, index) => {
+//     image.addEventListener("click", () => {
+//         popupImage?.classList.add("open");
+//         document.body.classList.add('no-scroll');
+//         showImage(index);
+//     });
+// });
+// popupImage?.addEventListener("click", () => {
+//     popupImgElement?.classList.remove('open');
+//     popupImgElement?.addEventListener('transitionend', () => {
+//         if(popupImgElement?.classList.contains('open')) return;
+//         popupImage?.classList.remove("open");
+//         document.body.classList.remove('no-scroll');
+//     });
+// });
+// function showImage(index) {
+//     popupImgElement.src = images[index].getAttribute('src');
+//     popupImgElement.classList.add('open');
+//     currentImageIndex = index;
+// }
 
 const button = document.querySelector('.dropbtn');
 const dropdown = document.querySelector('.dropdown');
--- src/scripts/img.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const images = document.querySelectorAll(".section2__img");
-
-images.forEach((image) => {
-  image.addEventListener("click", () => {
-    image.classList.toggle("expanded");
-  });
-});
